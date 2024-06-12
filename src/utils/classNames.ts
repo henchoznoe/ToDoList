8b9857f8ac72a2a112ca05996c71255f52603f13
Noé Henchoz <nhenchoz122@icloud.com>
2024-06-12 23:34:24 +0200

    Change classNames.ts

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,4 +1,4 @@
-import clsx, { ClassValue } from 'clsx'
+import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+export const cn = (...classes: string[]) => twMerge(clsx(...classes));
